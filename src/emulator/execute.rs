cd71375cd052bb5c906f99a60552a45175a78d9d
Anthony <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-02-15 23:19:31 -0800

    Implement I-type instruction execution

--- src/emulator/execute.rs
+++ src/emulator/execute.rs
@@ -11,7 +11,7 @@ use crate::instruction_set_definition::{
 use super::cpu::{
     memory::MemoryBus,
     registers::{RegisterFile32Bit, RegisterMapping},
-    Cpu32Bit,
+    Cpu32Bit, Size,
 };
 
 pub trait Execute32BitInstruction {
@@ -42,14 +42,24 @@ impl Execute32BitInstruction for Cpu32Bit {
                 funct3: _,
                 rs1,
                 imm,
-            } => execute_itype_instruction(
-                &mut self.registers,
-                &self.memory,
-                operation,
-                rd,
-                rs1,
-                imm,
-            ),
+            } => {
+                execute_itype_instruction(
+                    &mut self.debug,
+                    &mut self.pc,
+                    &mut self.registers,
+                    &self.memory,
+                    operation,
+                    rd,
+                    rs1,
+                    imm,
+                )?;
+                if let ITypeOperation::Jalr = operation {
+                    // if the instruction is a jalr, the program counter is already updated
+                    // by the execute_itype_instruction function
+                    return Ok(());
+                }
+                Ok(())
+            }
             Self::InstructionSet::RType {
                 operation,
                 rd,
@@ -99,13 +109,56 @@ impl Execute32BitInstruction for Cpu32Bit {
 }
 
 fn execute_itype_instruction(
-    _registers: &mut RegisterFile32Bit, // needs mutable access to the registers
-    _memory: &MemoryBus,                // needs immutable access to the memory
-    _operation: ITypeOperation,
-    _rd: RegisterMapping,
-    _rs1: RegisterMapping,
-    _imm: i32,
+    debug: &mut bool,
+    pc: &mut u32,
+    regs: &mut RegisterFile32Bit, // needs mutable access to the registers
+    memory: &MemoryBus,           // needs immutable access to the memory
+    operation: ITypeOperation,
+    rd: RegisterMapping,
+    rs1: RegisterMapping,
+    imm: i32,
 ) -> Result<()> {
+    match operation {
+        ITypeOperation::Addi => regs[rd] = regs[rs1].wrapping_add(imm as u32),
+        ITypeOperation::Andi => regs[rd] = regs[rs1] & (imm as u32),
+        ITypeOperation::Jalr => {
+            let t = *pc + 4;
+            *pc = (regs[rs1].wrapping_add(imm as u32) & !1) as u32;
+            regs[rd] = t;
+        }
+        ITypeOperation::Lb => {
+            regs[rd] = ((memory.read(regs[rs1].wrapping_add_signed(imm), Size::Byte)? as i32) << 24
+                >> 24) as u32
+        }
+        ITypeOperation::Lh => {
+            regs[rd] = ((memory.read(regs[rs1].wrapping_add_signed(imm), Size::Half)? as i32) << 16
+                >> 16) as u32
+        }
+        ITypeOperation::Lw => {
+            regs[rd] = memory.read(regs[rs1].wrapping_add_signed(imm), Size::Word)?
+        }
+        ITypeOperation::Ori => regs[rd] = regs[rs1] | (imm as u32),
+        ITypeOperation::Slli => regs[rd] = regs[rs1] << (imm & 0b11111),
+        ITypeOperation::Slti => regs[rd] = if (regs[rs1] as i32) < imm { 1 } else { 0 },
+        ITypeOperation::Sltiu => regs[rd] = if regs[rs1] < (imm as u32) { 1 } else { 0 },
+        ITypeOperation::Srai => regs[rd] = ((regs[rs1] as i32) >> (imm & 0b11111)) as u32,
+        ITypeOperation::Srli => regs[rd] = regs[rs1] >> (imm & 0b11111),
+        ITypeOperation::Xori => regs[rd] = regs[rs1] ^ (imm as u32),
+        ITypeOperation::Lbu => {
+            regs[rd] = memory.read(regs[rs1].wrapping_add_signed(imm), Size::Byte)?
+        }
+        ITypeOperation::Lhu => {
+            regs[rd] = memory.read(regs[rs1].wrapping_add_signed(imm), Size::Half)?
+        }
+        ITypeOperation::Fence => unimplemented!("fence instruction not implemented"),
+        ITypeOperation::FenceI => unimplemented!("fence.i instruction not implemented"),
+        ITypeOperation::Ecall => process_ecall(regs, memory)?,
+        ITypeOperation::Ebreak => *debug = true,
+    }
+    Ok(())
+}
+
+fn process_ecall(_registers: &mut RegisterFile32Bit, _memory: &MemoryBus) -> Result<()> {
     todo!()
 }
 
