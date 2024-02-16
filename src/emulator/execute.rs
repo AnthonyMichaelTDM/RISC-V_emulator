bb84c71aa8827ba8140b93288070651346b4830d
Anthony <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-02-16 01:53:17 -0800

    Add syscall processing

--- src/emulator/execute.rs
+++ src/emulator/execute.rs
@@ -312,7 +312,187 @@ fn execute_utype_instruction(
 ) -> Result<()> {
     match operation {
         UTypeOperation::Lui => registers[rd] = imm << 12,
-        UTypeOperation::Auipc => registers[rd] = pc + (imm << 12),
+        UTypeOperation::Auipc => registers[rd] = pc.wrapping_add(imm << 12),
     }
     Ok(())
+}
+
+/// Processes Syscalls (ecall) made by the program being executed.
+///
+/// # Arguments
+///
+/// * `registers` - The CPU's register file.
+///
+/// # Register Usage
+///
+/// * `a7` - The syscall number.
+/// * `a0` - The first argument to the syscall.
+/// * `a1` - The second argument to the syscall.
+/// * `a2` - The third argument to the syscall.
+/// * `a3` - The fourth argument to the syscall.
+///
+/// # Register Updates
+///
+/// * `a0` - The return value of the syscall.
+fn process_ecall(
+    regs: &mut RegisterFile32Bit,
+    memory: &mut MemoryBus,
+    output: &mut String,
+) -> Result<()> {
+    match Syscall::from(regs[RegisterMapping::A7] as u8) {
+        Syscall::PrintInt => output.push_str(&regs[RegisterMapping::A0].to_string()),
+        Syscall::PrintString => {
+            let mut addr = regs[RegisterMapping::A0];
+            loop {
+                let byte = memory.read(addr, Size::Byte).map_err(|e| {
+                    anyhow::anyhow!(
+                        "Error reading string from memory at address{}: {}",
+                        regs[RegisterMapping::A0],
+                        e
+                    )
+                })?;
+                if byte == 0 {
+                    break;
+                }
+                output.push(byte as u8 as char);
+                addr += 1;
+            }
+        }
+        Syscall::ReadInt => {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input)?;
+            let value = input.trim().parse::<i32>()?;
+            regs[RegisterMapping::A0] = value as u32;
+        }
+        Syscall::ReadString => {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input)?;
+
+            let addr = regs[RegisterMapping::A0];
+            let max_len = regs[RegisterMapping::A1] as usize;
+            let mut i = 0;
+            for byte in input.bytes() {
+                if i >= max_len - 1 {
+                    break;
+                }
+                memory.write(addr + i as u32, byte as u32, Size::Byte)?;
+                i += 1;
+            }
+            // ensure the last byte is the null terminator
+            memory.write(addr + i as u32, 0, Size::Byte)?;
+        }
+        Syscall::Exit => bail!("Program exited with code: 0"),
+        Syscall::PrintChar => output.push(char::from(regs[RegisterMapping::A0] as u8)),
+        Syscall::ReadChar => {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input)?;
+            let value = input.trim().chars().next().unwrap() as u8;
+            regs[RegisterMapping::A0] = value as u32;
+        }
+        Syscall::Time => {
+            let time = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .map_err(|e| anyhow::anyhow!("Error getting time: {}", e))?;
+            regs[RegisterMapping::A0] = time.as_millis() as u32;
+            regs[RegisterMapping::A1] = (time.as_millis() >> 32) as u32;
+        }
+        Syscall::Sleep => {
+            let duration = std::time::Duration::from_millis(regs[RegisterMapping::A0] as u64);
+            std::thread::sleep(duration);
+        }
+        Syscall::PrintIntHex => output.push_str(&format!("{:#x}", regs[RegisterMapping::A0])),
+        Syscall::PrintIntBinary => output.push_str(&format!("{:#b}", regs[RegisterMapping::A0])),
+        Syscall::PrintIntUnsigned => output.push_str(&regs[RegisterMapping::A0].to_string()),
+        Syscall::Exit2 => bail!("Program exited with code: {}", regs[RegisterMapping::A0]),
+        Syscall::UnSupported => bail!("Unsupported syscall number: {}", regs[RegisterMapping::A7]),
+    }
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[repr(u8)]
+enum Syscall {
+    /// Print an integer to the console.
+    /// # Inputs:
+    /// a0 - the integer to print
+    PrintInt = 1,
+    // PrintFloat = 2,
+    // PrintDouble = 3,
+    /// Print a string to the console.
+    /// # Inputs:
+    /// a0 - the address of the null-terminated string to print
+    PrintString = 4,
+    /// Read an integer from the console.
+    /// # Outputs:
+    /// a0 - the integer read from the console
+    ReadInt = 5,
+    // ReadFloat = 6,
+    // ReadDouble = 7,
+    /// Read a string from the console.
+    /// # Inputs:
+    /// a0 - the address of the buffer to read the string into
+    /// a1 - the maximum number of characters to read
+    ReadString = 8,
+    /// Exit the program with code 0
+    Exit = 10,
+    /// Print an ascii character to the console.
+    /// # Inputs:
+    /// a0 - the ascii character to print (only the lower 8 bits are used)
+    PrintChar = 11,
+    /// Read an ascii character from the console.
+    /// # Outputs:
+    /// a0 - the ascii character read from the console
+    ReadChar = 12,
+    /// get the current Unix time (milliseconds since 1 January 1970)
+    /// # Outputs:
+    /// a0 - lower order 32-bits of the time
+    /// a1 - upper order 32-bits of the time
+    Time = 30,
+    /// Sleep for the given number of milliseconds
+    /// # Inputs:
+    /// a0 - the number of milliseconds to sleep
+    Sleep = 32,
+    /// Print an integer to the console in hexadecimal format.
+    /// # Inputs:
+    /// a0 - the integer to print
+    PrintIntHex = 34,
+    /// Print an integer to the console in binary format.
+    /// # Inputs:
+    /// a0 - the integer to print
+    PrintIntBinary = 35,
+    /// Print an integer to the console in unsigned format.
+    /// # Inputs:
+    /// a0 - the integer to print
+    PrintIntUnsigned = 36,
+    // RandSeed = 40,
+    // RandInt = 41,
+    // RandIntRange = 42,
+    // RandFloat = 43,
+    // RandDouble = 44,
+    /// Exit the program with the given exit code
+    /// # Inputs:
+    /// a0 - the exit code
+    Exit2 = 93,
+    UnSupported,
+}
+
+impl From<u8> for Syscall {
+    fn from(value: u8) -> Self {
+        match value {
+            1 => Syscall::PrintInt,
+            4 => Syscall::PrintString,
+            5 => Syscall::ReadInt,
+            8 => Syscall::ReadString,
+            10 => Syscall::Exit,
+            11 => Syscall::PrintChar,
+            12 => Syscall::ReadChar,
+            30 => Syscall::Time,
+            32 => Syscall::Sleep,
+            34 => Syscall::PrintIntHex,
+            35 => Syscall::PrintIntBinary,
+            36 => Syscall::PrintIntUnsigned,
+            93 => Syscall::Exit2,
+            _ => Syscall::UnSupported,
+        }
+    }
 }
